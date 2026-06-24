1c181aa66da58479e7f311a0197cbd66023b9cf2
alexander.zhuravlev <zhur06@gmail.com>
2026-06-24 13:19:04 +0200

    implemented conditionals

--- stack-my-lang/src/MyCodeGen.hs
+++ stack-my-lang/src/MyCodeGen.hs
@@ -4,10 +4,9 @@ module MyCodeGen
     ( codeGen ) where
 
 import Sprockell
-import ParseJSON (AST(..), Coordinate(..))
+import ParseJSON (AST(..), Coordinate(..), ElseIfBranch(..))
 import Data.HashMap (Map, insert)
 import qualified Data.HashMap as Map
-import Data.Maybe (fromJust)
 
 -- codeGen :: Integer -> [Instruction]
 -- codeGen n = [ 
@@ -49,12 +48,44 @@ getOrCreateAddress Coordinate {level, offset} varType symbolTable freeAddress =
                        newSymbolTable = insert level newAddress symbolTable
                    in (newAddress + fromInteger offset, newSymbolTable, newAddress + sizeOfType varType)
 
+constructStatements :: [AST] -> SymbolTable -> Address -> ([Instruction], SymbolTable, Address)
+constructStatements children symbolTable freeAddress =
+    foldl
+        (\(prevInstr, prevSymTable, prevFreeAddr) cur ->
+            let (curInstr, curSymTable, curFreeAddr) = constructProgram cur prevSymTable prevFreeAddr
+            in (prevInstr ++ curInstr, curSymTable, curFreeAddr)
+        )
+        ([], symbolTable, freeAddress)
+        children
+
+constructConditionalClauses
+    :: [(AST, [AST])]
+    -> [Instruction]
+    -> SymbolTable
+    -> Address
+    -> ([Instruction], SymbolTable, Address)
+constructConditionalClauses [] elseInstr symbolTable freeAddress = (elseInstr, symbolTable, freeAddress)
+constructConditionalClauses ((condAst, children) : restClauses) elseInstr symbolTable freeAddress =
+    let (condInstr, condSymTable, condFreeAddr) = constructProgram condAst symbolTable freeAddress
+        (childrenInstr, childrenSymTable, childrenFreeAddr) = constructStatements children condSymTable condFreeAddr
+        (restInstr, restSymTable, restFreeAddr) =
+            constructConditionalClauses restClauses elseInstr childrenSymTable childrenFreeAddr
+        falseJumpOffset = length childrenInstr + 2
+        endJumpOffset = length restInstr + 1
+        instructions =
+            condInstr
+                ++ [ Pop regA
+                   , Branch regA (Rel 2)
+                   , Jump (Rel falseJumpOffset)
+                   ]
+                ++ childrenInstr
+                ++ [Jump (Rel endJumpOffset)]
+                ++ restInstr
+    in (instructions, restSymTable, restFreeAddr)
+
 constructProgram :: AST -> SymbolTable -> Address -> ([Instruction], SymbolTable, Address)
 constructProgram (Program children) symbolTable freeAddress =
-        foldl (\(prevInstr, prevSymTable, prevFreeAddr) cur ->
-                    let (curInstr, curSymTable, curFreeAddr) = constructProgram cur prevSymTable prevFreeAddr
-                    in (prevInstr ++ curInstr, curSymTable, curFreeAddr)
-                ) ([], symbolTable, freeAddress) children
+    constructStatements children symbolTable freeAddress
 constructProgram (Decl {declName, declType, declValue, declCoordinate}) symbolTable freeAddress =
     let (declAddress, newSymbolTable, nextFreeAddress) = getOrCreateAddress declCoordinate declType symbolTable freeAddress
     in
@@ -103,9 +134,15 @@ constructProgram (BinaryOp {opName, leftOperand, rightOperand}) symbolTable free
             "or" -> Compute Or regA regB regA
             _ -> error "Unknown operator"
     in (leftInstr ++ rightInstr ++ [Pop regB, Pop regA, opInstr, Push regA], rightSymTable, rightFreeAddr)
+constructProgram (If {ifCond, ifChildren, ifElifs, ifElse}) symbolTable freeAddress =
+    let clauses = (ifCond, ifChildren) : map (\ElseIfBranch {elifCond, elifChildren} -> (elifCond, elifChildren)) ifElifs
+        elseChildren = maybe [] id ifElse
+        (elseInstr, elseSymTable, elseFreeAddr) = constructStatements elseChildren symbolTable freeAddress
+    in constructConditionalClauses clauses elseInstr elseSymTable elseFreeAddr
 constructProgram (Print printValue) symbolTable freeAddress =
     let (instructions, newSymbolTable, newFreeAddress) = constructProgram printValue symbolTable freeAddress
     in (instructions ++ [
+        Pop regA,
         WriteInstr regA numberIO
     ], newSymbolTable, newFreeAddress)
 
