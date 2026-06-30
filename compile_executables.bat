d39d924886fe053423ecc12ddebb4e78d0b4f4d9
LACHIE KEANE <lachlankeane014@gmail.com>
2026-06-30 19:07:37 +0200

    Add Windows batch file

---
+++ compile_executables.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if not exist "bin" (
+  echo Creating bin directory
+  mkdir bin
+)
+
+call mvn clean install -q -Dmaven.test.redirectTestOutputToFile=true
+if errorlevel 1 (
+  echo FATAL ERROR: Frontend compilation failed
+  exit /b 1
+)
+echo INFO: Frontend compiled successfully
+
+for /f "delims=" %%A in ('call mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout') do set frontendArtifact=%%A
+for /f "delims=" %%A in ('call mvn help:evaluate -Dexpression=project.version -q -DforceStdout') do set frontendVersion=%%A
+set frontendExecutable=%frontendArtifact%-%frontendVersion%.jar
+
+echo INFO: Copying frontend executable
+copy /Y "target\%frontendExecutable%" "bin\frontend.jar" >nul
+
+cd stack-my-lang
+call stack install
+
+if errorlevel 1 (
+  echo FATAL ERROR: Backend compilation failed
+  cd ..
+  exit /b 1
+)
+echo INFO: Backend compiled successfully
+echo INFO: Copying backend executable
+copy /Y "%USERPROFILE%\AppData\Roaming\local\bin\my-lang-exe.exe" "..\bin\backend.exe" >nul
+
+cd ..
+echo Exiting
